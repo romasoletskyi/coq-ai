fcf850daa0764643d7ec08341362276c8ed9227a
Roman Soletskyi <romansoletskyi@gmail.com>
2023-11-01 16:41:50 +0100

    nat add commutativity example

--- coq_parser/tests/project/test.v
+++ coq_parser/tests/project/test.v
@@ -1,7 +1,23 @@
-Theorem plus_n_Sm : forall n m : nat,
-  S (n + m) = n + (S m).
+Lemma plus_n_Sm : forall n m : nat,
+  n + (S m) = S (n + m).
 Proof.
   intros n m. induction n as [| n' IHn'].
   - simpl. reflexivity.
-  - simpl. rewrite -> IHn'. reflexivity.
-Qed.  
+  - simpl. rewrite IHn'. reflexivity.
+Qed. 
+
+Theorem plus_n_0 : forall n : nat,
+  n + 0 = n.
+Proof.
+  intro n. induction n as [|n' IHn'].
+  - reflexivity.
+  - simpl. rewrite IHn'. reflexivity.
+Qed.
+
+Theorem plus_comm : forall n m : nat,
+  n + m = m + n.
+Proof.  
+  intros n m. induction n as [|n' IHn'].
+  - rewrite plus_n_0. reflexivity.
+  - rewrite plus_n_Sm. rewrite <- IHn'. reflexivity.
+Qed.
